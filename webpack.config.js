83154f7ee89961c86011ab7ff3e8a3fa750c3c83
Dejmian11 <dam.pru@o2.pl>
2022-09-12 09:52:56 +0200

    Menu, stats and info sections added

--- webpack.config.js
+++ webpack.config.js
@@ -117,14 +117,14 @@
              ignore: ['*.DS_Store', 'Thumbs.db'],
            },
          },
-        //  {
-          //  from: path.resolve(environment.paths.source, 'videos'),
-          //  to: path.resolve(environment.paths.output, 'videos'),
-          //  toType: 'dir',
-          //  globOptions: {
-          //    ignore: ['*.DS_Store', 'Thumbs.db'],
-          //  },
-        //  },
+         {
+           from: path.resolve(environment.paths.source, 'videos'),
+           to: path.resolve(environment.paths.output, 'videos'),
+           toType: 'dir',
+           globOptions: {
+             ignore: ['*.DS_Store', 'Thumbs.db'],
+           },
+         },
        ],
      }),
    ].concat(htmlPluginEntries),
