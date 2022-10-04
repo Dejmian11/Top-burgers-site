46df0d36aec93446e188fbe3824255ed42b38837
Dejmian11 <dam.pru@o2.pl>
2022-10-04 09:11:24 +0200

    Sticky header on scroll added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
 import '../scss/main.scss';
+
+// ========================== STICKY HEADER ON SCROLL ==========================
+window.addEventListener('scroll', () => {
+	const header = document.querySelector('.header');
+
+	header.classList.toggle('header--scrolled', window.scrollY > 0); 
+});
