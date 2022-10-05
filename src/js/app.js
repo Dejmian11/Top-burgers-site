01852e60c259dd4f533255883a0e34ba51dcadfd
Dejmian11 <dam.pru@o2.pl>
2022-10-05 16:28:15 +0200

    Sidebar added

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,52 @@
 import '../scss/main.scss';
 
+const header = document.querySelector('.header');
+
 // ========================== STICKY HEADER ON SCROLL ==========================
 window.addEventListener('scroll', () => {
-	const header = document.querySelector('.header');
-
-	header.classList.toggle('header--scrolled', window.scrollY > 0); 
+	header.classList.toggle('header--scrolled', window.scrollY > 0);
 });
+
+// ========================== SIDEBAR ==========================
+const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+const headerSidebar = document.querySelector('.header-sidebar');
+
+// Show sidebar nav
+const showSidebarNav = () => {
+	headerSidebar.hasAttribute('data-visible')
+		? mobileNavToggle.setAttribute('aria-expanded', false)
+		: mobileNavToggle.setAttribute('aria-expanded', true);
+
+	headerSidebar.toggleAttribute('data-visible');
+
+	changeMobileNavToggleIcon(headerSidebar);
+};
+
+mobileNavToggle.addEventListener('click', showSidebarNav);
+
+// Close the sidebar when the viewport is bigger
+window.addEventListener('resize', () => {
+	if (window.innerWidth >= 992) {
+		headerSidebar.removeAttribute('data-visible');
+	}
+
+  changeMobileNavToggleIcon(headerSidebar);
+});
+
+// Change mobile nav toggle icon
+const changeMobileNavToggleIcon = headerSidebar => {
+	const mobileNavToggleIcon = mobileNavToggle.querySelector('use');
+
+	if (headerSidebar.hasAttribute('data-visible'))
+		mobileNavToggleIcon.setAttribute(
+			'xlink:href',
+			'images/icons/sprite.svg#icon-cross'
+		);
+
+	if (!headerSidebar.hasAttribute('data-visible'))
+		mobileNavToggleIcon.setAttribute(
+			'xlink:href',
+			'images/icons/sprite.svg#icon-menu'
+		);
+};
+
