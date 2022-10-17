982ad901d9dd635c89675319de28cc27f373b047
Dejmian11 <dam.pru@o2.pl>
2022-10-17 11:28:12 +0200

    Smooth scrolling fixed

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,77 @@ import '../scss/main.scss';
 
 const header = document.querySelector('.header');
 
-// ========================== STICKY HEADER ON SCROLL ==========================
-window.addEventListener('scroll', () => {
-	header.classList.toggle('header--scrolled', window.scrollY > 0);
-});
+// ========================== HEADER AND NAVIGATION ==========================
+const homeSection = document.querySelector('.home');
+
+// STICKY HEADER ON SCROLL
+const homeSectionOptions = {
+	threshold: 0.95,
+};
+
+const homeSectionObserver = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) header.classList.add('header--scrolled');
+		if (entry.isIntersecting) header.classList.remove('header--scrolled');
+	});
+}, homeSectionOptions);
+
+homeSectionObserver.observe(homeSection);
+
+// SECTION INDICATOR
+const sections = document.querySelectorAll('.section');
+const navLinks = document.querySelectorAll('.nav__link');
+
+const sectionsOptions = {
+	root: null,
+	threshold: 0,
+	rootMargin: '-350px',
+};
+
+const sectinonsObserver = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (!entry.isIntersecting) return;
+
+		console.log(entry);
+
+		navLinks.forEach(navLink => {
+			navLink.classList.remove('active');
+
+			if (navLink.getAttribute('href').split('#')[1] === entry.target.id)
+				navLink.classList.toggle('active');
+		});
+	});
+}, sectionsOptions);
+
+sections.forEach(section => sectinonsObserver.observe(section));
+
+// const navLinks = document.querySelectorAll('.nav__link');
+
+// const sectionOptions = {};
+
+// const sectionObserver = new IntersectionObserver(entries => {
+// 	entries.forEach(entry => {
+// 		navLinks.forEach(navLink => {
+// 			// console.log(entry.target.id);
+// 			// console.log(navLink.getAttribute('href').split('#')[1]);
+// 			console.log(entry.target.offsetHeight);
+
+// 			if (
+// 				entry.target.id === navLink.getAttribute('href').split('#')[1] &&
+// 				window.scrollY >= entry.target.offsetTop && window.scrollY < entry.target.offsetTop + entry.target.offsetHeight
+// 			){
+// 				navLink.classList.remove('active');
+//         navLink.
+//       }else return;
+// 		});
+// 	});
+// }, sectionOptions);
+
+// sections.forEach(section => {
+// 	sectionObserver.observe(section);
+// });
+
+// console.log(homeSectionObserver);
 
 // ========================== SIDEBAR ==========================
 const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
@@ -51,32 +118,47 @@ const changeMobileNavToggleIcon = headerSidebar => {
 };
 
 // ========================== SMOOTH SCROLLING ==========================
-const homeSectionBtn = document.querySelector('.home__btn');
-const scrollBtn = document.querySelector('.home__scroll-link');
-const navLinks = document.querySelectorAll('.nav__link');
+const headerHeight = header.offsetHeight;
+const headerContainer = header.querySelector('.header-container');
+const headerContainerPadding =
+	headerContainer.getPropertyValue('padding-block');
 
-const scrollToSectionBtns = [homeSectionBtn, scrollBtn, ...navLinks];
+console.log(headerContainerPadding);
 
-console.log(scrollToSectionBtns);
+// Get scrolled header height
+const scrolledHeaderHeight = headerHeight - headerContainerPadding;
 
-const scrollToSection = e => {
-	const btnHref = e.target.getAttribute('href');
-	const targetSection = document.querySelector(btnHref);
-	const targetSectionCoords = targetSection.getBoundingClientRect();
-	const headerHeight = document.querySelector('.header').offsetHeight;
+// Include header height
+document.documentElement.style.setProperty(
+	'--scroll-padding',
+	`${scrolledHeaderHeight}`
+);
+// const homeSectionBtn = document.querySelector('.home__btn');
+// const scrollBtn = document.querySelector('.home__scroll-link');
+// const navLinks = document.querySelectorAll('.nav__link');
 
-	console.log(targetSectionCoords.top);
-	console.log(window.scrollY);
-	console.log(headerHeight);
+// const scrollToSectionBtns = [homeSectionBtn, scrollBtn, ...navLinks];
 
-	// window.scrollTo(
-	// 	targetSectionCoords.left + window.scrollX,
-	// 	targetSectionCoords.top + window.scrollY - headerHeight
-	// );
+// console.log(scrollToSectionBtns);
 
-  targetSection.scrollIntoView();
-};
+// const scrollToSection = e => {
+// 	const btnHref = e.target.getAttribute('href');
+// 	const targetSection = document.querySelector(btnHref);
+// 	const targetSectionCoords = targetSection.getBoundingClientRect();
+// 	const headerHeight = document.querySelector('.header').offsetHeight;
 
-scrollToSectionBtns.forEach(scrollToSectionBtn => {
-	scrollToSectionBtn.addEventListener('click', scrollToSection);
-});
+// 	console.log(targetSectionCoords.top);
+// 	console.log(window.scrollY);
+// 	console.log(headerHeight);
+
+// 	// window.scrollTo(
+// 	// 	targetSectionCoords.left + window.scrollX,
+// 	// 	targetSectionCoords.top + window.scrollY - headerHeight
+// 	// );
+
+//   targetSection.scrollIntoView();
+// };
+
+// scrollToSectionBtns.forEach(scrollToSectionBtn => {
+// 	scrollToSectionBtn.addEventListener('click', scrollToSection);
+// });
